9171a24b3320a1255f97b47dc33b4b5317c93077
Marialuizadamasceno <marialuizadamceno@gmail.com>
2023-10-04 18:11:01 -0300

    tarefa

--- tarefa25.py
+++ tarefa25.py
@@ -8,29 +8,24 @@ def cadastrar_cliente(clientes, nome , email, telefone):
     clientes.append(cliente)
     print("suscesso!")
 clientes=[] 
-nome = input("nome do cliente: ")
-email = input("Email do cliente: ")
-telefone = input("telefone do cliente: ")
-cadastrar_cliente(clientes,nome,email,telefone)
 
-def ler_dados_csv():
-    with open('clientes.csv', mode='r') as arquivo_csv:
-        leitor_csv = csv.DictReader(arquivo_csv)
-        for linha in leitor_csv:
-            print(f"Nome:{linha['Nome']}, Email: {linha['Email']}, Telefone:{['Telefone']}")
-            cadastrar_cliente(clientes,nome,email,telefone)
-
-def gravar_dado():
-    
 
 def criar_arquivo_csv():
-    with open('arquivo_csv', mode="w", newline="") as arquivo_csv:
+    with open('arquivo.csv', mode="w", newline="") as arquivo_csv:
         writer=csv.writer(arquivo_csv) #criar um novo arquivo
         writer.writerow(["Nome","Email","Telefone"])
     
         for clientee in clientes: #para navegar no dicionário
              writer.writerow([clientee["Nome"], clientee["Email"], clientee["Telefone"]])
              print("salvo!")
+
+def ler_dados_csv():
+    with open('arquivo.csv', mode='r') as arquivo_csv:
+        leitor_csv = csv.DictReader(arquivo_csv)
+        for linha in leitor_csv:
+            print(f"Nome:{linha['Nome']}, Email: {linha['Email']}, Telefone:{linha['Telefone']}")
+
+
 
 while True:
     print("MENU\n"
@@ -44,8 +39,7 @@ while True:
         email = input("Email do cliente: ")
         telefone = input("telefone do cliente: ")
         cadastrar_cliente(clientes,nome,email,telefone)
+        criar_arquivo_csv()
     elif opcao == 2:
         ler_dados_csv()
-
-    elif opcao == 3:
-        print("salvo com sescesso!")
+        
